11097340122278bcd13661e2be5174e76604f5fd
Kai Liu (Victor) <vic.kai.liu@gmail.com>
2014-12-21 19:52:48 +0800

    move all the script in front

--- dashboard.php
+++ dashboard.php
@@ -3,6 +3,86 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- js placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"/>
+    <script src="js/jquery-1.8.3.min.js"/>
+    <script src="js/bootstrap.min.js"/>
+    <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"/>
+    <script src="js/jquery.scrollTo.min.js"/>
+    <script src="js/jquery.nicescroll.js" type="text/javascript"/>
+    <script src="js/jquery.sparkline.js"/>
+
+    <!--common script for all pages-->
+    <script src="js/common-scripts.js"/>
+
+    <script type="text/javascript" src="js/gritter/js/jquery.gritter.js"/>
+    <script type="text/javascript" src="js/gritter-conf.js"/>
+
+    <!--script for this page-->
+    <script src="js/sparkline-chart.js"/>
+    <script src="js/zabuto_calendar.js"/>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            var unique_id = $.gritter.add({
+                // (string | mandatory) the heading of the notification
+                title: 'Moblie Learning Update',
+                // (string | mandatory) the text inside the notification
+                text: 'SEEP app is now available on iOS and Google Play. Click <b>here</b> to download app.',
+                // (string | optional) the image to display on the left
+                image: 'img/friends/fr-09.jpg',
+                // (bool | optional) if you want it to fade out on its own or just sit there
+                sticky: true,
+                // (int | optional) the time you want it to be alive for before fading out
+                time: '',
+                // (string | optional) the class name you want to apply to that specific message
+                class_name: 'my-sticky-class'
+            });
+            return false;
+        });
+    </script>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $("#date-popover").popover({
+                html: true,
+                trigger: "manual"
+            });
+            $("#date-popover").hide();
+            $("#date-popover").click(function(e) {
+                $(this).hide();
+            });
+
+            $("#my-calendar").zabuto_calendar({
+                action: function() {
+                    return myDateFunction(this.id, false);
+                },
+                action_nav: function() {
+                    return myNavFunction(this.id);
+                },
+                ajax: {
+                    url: "show_data.php?action=1",
+                    modal: true
+                },
+                legend: [{
+                    type: "text",
+                    label: "Special event",
+                    badge: "00"
+                }, {
+                    type: "block",
+                    label: "Regular event",
+                }]
+            });
+        });
+
+        function myNavFunction(id) {
+            $("#date-popover").hide();
+            var nav = $("#" + id).data("navigation");
+            var to = $("#" + id).data("to");
+            console.log('nav ' + nav + ' to: ' + to.month + '/' + to.year);
+        }
+    </script>
+
     <script>
         function getHiddenProp() {
             var prefixes = ['webkit', 'moz', 'ms', 'o'];
@@ -517,84 +597,6 @@
         <!--footer end-->
     </section>
 
-    <!-- js placed at the end of the document so the pages load faster -->
-    <script src="js/jquery.js"/>
-    <script src="js/jquery-1.8.3.min.js"/>
-    <script src="js/bootstrap.min.js"/>
-    <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"/>
-    <script src="js/jquery.scrollTo.min.js"/>
-    <script src="js/jquery.nicescroll.js" type="text/javascript"/>
-    <script src="js/jquery.sparkline.js"/>
 
-    <!--common script for all pages-->
-    <script src="js/common-scripts.js"/>
-
-    <script type="text/javascript" src="js/gritter/js/jquery.gritter.js"/>
-    <script type="text/javascript" src="js/gritter-conf.js"/>
-
-    <!--script for this page-->
-    <script src="js/sparkline-chart.js"/>
-    <script src="js/zabuto_calendar.js"/>
-
-    <script type="text/javascript">
-        $(document).ready(function() {
-            var unique_id = $.gritter.add({
-                // (string | mandatory) the heading of the notification
-                title: 'Moblie Learning Update',
-                // (string | mandatory) the text inside the notification
-                text: 'SEEP app is now available on iOS and Google Play. Click <b>here</b> to download app.',
-                // (string | optional) the image to display on the left
-                image: 'assets/img/friends/fr-09.jpg',
-                // (bool | optional) if you want it to fade out on its own or just sit there
-                sticky: true,
-                // (int | optional) the time you want it to be alive for before fading out
-                time: '',
-                // (string | optional) the class name you want to apply to that specific message
-                class_name: 'my-sticky-class'
-            });
-            return false;
-        });
-    </script>
-
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $("#date-popover").popover({
-                html: true,
-                trigger: "manual"
-            });
-            $("#date-popover").hide();
-            $("#date-popover").click(function(e) {
-                $(this).hide();
-            });
-
-            $("#my-calendar").zabuto_calendar({
-                action: function() {
-                    return myDateFunction(this.id, false);
-                },
-                action_nav: function() {
-                    return myNavFunction(this.id);
-                },
-                ajax: {
-                    url: "show_data.php?action=1",
-                    modal: true
-                },
-                legend: [{
-                    type: "text",
-                    label: "Special event",
-                    badge: "00"
-                }, {
-                    type: "block",
-                    label: "Regular event",
-                }]
-            });
-        });
-
-        function myNavFunction(id) {
-            $("#date-popover").hide();
-            var nav = $("#" + id).data("navigation");
-            var to = $("#" + id).data("to");
-            console.log('nav ' + nav + ' to: ' + to.month + '/' + to.year);
-        }
-    </script>
 </body>
 </html>
